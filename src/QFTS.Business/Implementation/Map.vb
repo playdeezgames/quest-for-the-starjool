98d1663c524ab81e2f10cac1eaa12a50a1f9b537
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-27 05:22:13 -0600

    Map

--- src/QFTS.Business/Implementation/Map.vb
+++ src/QFTS.Business/Implementation/Map.vb
@@ -1,14 +1,11 @@
 ﻿Friend Class Map
     Implements IMap
-
     Private _worldData As WorldData
     Private _data As MapData
-
     Public Sub New(worldData As WorldData, mapData As MapData)
         Me._worldData = worldData
         Me._data = mapData
     End Sub
-
     Public Function GetCell(mapColumn As Integer, mapRow As Integer) As IMapCell Implements IMap.GetCell
         If mapColumn < 0 OrElse mapRow < 0 OrElse mapColumn >= _data.Columns OrElse mapRow >= _data.Rows Then
             Return Nothing
