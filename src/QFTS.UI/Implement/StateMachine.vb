8e6753f9d09e2d2761cfd02b0f1218a5339d88c9
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-29 04:57:02 -0600

    StateMachine

--- src/QFTS.UI/Implement/StateMachine.vb
+++ src/QFTS.UI/Implement/StateMachine.vb
@@ -1,5 +1,3 @@
-Imports QFTS.Business
-
 Public Class StateMachine
     Implements IStateMachine
     Private ReadOnly _world As IWorld
@@ -21,38 +19,20 @@ Public Class StateMachine
         _states.Add(State.Navigation, New NavigationState(world, Me, _textGrid))
         State = State.Splash
     End Sub
-
     Public Sub Update(elapsed As TimeSpan) Implements IStateMachine.Update
         _states(_state).Update(elapsed)
-        '_textGrid.Fill(1,0,51,1,&hcd,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(1,29,51,1,&hcd,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(1,24,51,1,&hcd,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(0,1,1,23,&hba,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(52,1,1,23,&hba,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(0,25,1,4,&hba,Hue.Brown, Hue.Black)
-        '_textGrid.Fill(52,25,1,4,&hba,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(0,0).Plot(&hc9,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(52,0).Plot(&hbb,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(0,24).Plot(&hcc,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(52,24).Plot(&hb9,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(0,29).Plot(&hc8,Hue.Brown, Hue.Black)
-        '_textGrid.GetCell(52,29).Plot(&hbc,Hue.Brown, Hue.Black)
     End Sub
-
     Public Sub HandleKey(keyName As String) Implements IStateMachine.HandleKey
         _states(_state).HandleKey(keyName)
     End Sub
-
     Public Sub Reset() Implements IStateMachine.Reset
         For Each entry In _states
             entry.Value.Reset()
         Next
     End Sub
-
     Public Sub Quit() Implements IState.Quit
         _quit()
     End Sub
-
     Public Sub SetState(state As State) Implements IState.SetState
         If state = _state Then
             Return
@@ -60,11 +40,10 @@ Public Class StateMachine
         _state = state
         _states(_state).Reset()
     End Sub
-
     Private _state As State
-    Public Property State as State Implements IStateMachine.State
+    Public Property State As State Implements IStateMachine.State
         Get
-            return _state            
+            Return _state
         End Get
         Set(value As State)
             SetState(value)
