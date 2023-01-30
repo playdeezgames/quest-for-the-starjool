18a20b91efc5e2734245650b3fcdb9afa495ef07
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 04:23:57 -0600

    triggers(at least teleport, which is theonly one, works)

--- src/QFTS.Business/Implementation/Map.vb
+++ src/QFTS.Business/Implementation/Map.vb
@@ -10,7 +10,10 @@
     Public ReadOnly Property Name As String Implements IMap.Name
     Public Function GetCell(mapColumn As Integer, mapRow As Integer) As IMapCell Implements IMap.GetCell
         If mapColumn < 0 OrElse mapRow < 0 OrElse mapColumn >= _data.Columns OrElse mapRow >= _data.Rows Then
-            Return Nothing
+            Return New MapCell(_worldData, New MapCellData With
+                               {
+                                .Terrain = _data.DefaultTerrain
+                               })
         End If
         Return New MapCell(_worldData, _data.Cells(mapColumn + mapRow * _data.Columns))
     End Function
