d6b65db1e83bce4cb646fe7d5467b7ce6f69a301
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 14:45:55 -0600

    buy and sell stubs

--- src/QFTS.UI/Implement/ShoppeSellState.vb
+++ src/QFTS.UI/Implement/ShoppeSellState.vb
@@ -4,9 +4,15 @@
         MyBase.New(world, stateMachine, textGrid)
     End Sub
     Public Overrides Sub Update(elapsed As TimeSpan)
-        Throw New NotImplementedException()
+    End Sub
+    Public Overrides Sub HandleKey(keyName As String)
+        Select Case keyName
+            Case Escape
+                SetState(State.ShoppeWelcome)
+        End Select
     End Sub
     Public Overrides Sub Reset()
-        Throw New NotImplementedException()
+        TextGrid.FillAll(0, Hue.White, Hue.Black)
+        TextGrid.WriteText(0, 0, "Sell", Hue.White, Hue.Black)
     End Sub
 End Class
