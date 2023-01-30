e3c3b7360a9a64d3ad61b99e87c9b6b28222b646
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 05:48:46 -0600

    mapcell

--- src/QFTS.Business/Implementation/MapCell.vb
+++ src/QFTS.Business/Implementation/MapCell.vb
@@ -1,44 +1,42 @@
 ﻿Friend Class MapCell
+    Inherits Thingie(Of MapCellData)
     Implements IMapCell
-    Private _worldData As WorldData
-    Private _data As MapCellData
     Public Sub New(worldData As WorldData, mapCellData As MapCellData)
-        _worldData = worldData
-        _data = mapCellData
+        MyBase.New(worldData, mapCellData)
     End Sub
     Public ReadOnly Property Terrain As TerrainType Implements IMapCell.Terrain
         Get
-            Return _data.Terrain
+            Return Data.Terrain
         End Get
     End Property
     Public Property Character As ICharacter Implements IMapCell.Character
         Get
-            If _data.Character Is Nothing Then
+            If Data.Character Is Nothing Then
                 Return Nothing
             End If
-            Return New Character(_worldData, _data.Character)
+            Return New Character(WorldData, Data.Character)
         End Get
         Set(value As ICharacter)
             If value Is Nothing Then
-                _data.Character = Nothing
+                Data.Character = Nothing
                 Return
             End If
-            _data.Character = DirectCast(value, Character).Data
+            Data.Character = DirectCast(value, Character).Data
         End Set
     End Property
     Public ReadOnly Property HasCharacter As Boolean Implements IMapCell.HasCharacter
         Get
-            Return _data.Character IsNot Nothing
+            Return Data.Character IsNot Nothing
         End Get
     End Property
     Public ReadOnly Property CanHaveCharacter As Boolean Implements IMapCell.CanHaveCharacter
         Get
-            Return _data.Terrain.ToDescriptor.CanHaveCharacter
+            Return Data.Terrain.ToDescriptor.CanHaveCharacter
         End Get
     End Property
     Public ReadOnly Property Triggers As IEnumerable(Of ITrigger) Implements IMapCell.Triggers
         Get
-            Return _data.Triggers.Select(Function(x) New Trigger(_worldData, x))
+            Return Data.Triggers.Select(Function(x) New Trigger(WorldData, x))
         End Get
     End Property
 End Class
