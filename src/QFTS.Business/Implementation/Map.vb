455bdd775db96d0b245de0f57e011d52f50fbc28
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 05:47:29 -0600

    map!

--- src/QFTS.Business/Implementation/Map.vb
+++ src/QFTS.Business/Implementation/Map.vb
@@ -1,20 +1,18 @@
 ﻿Friend Class Map
+    Inherits Thingie(Of MapData)
     Implements IMap
-    Private ReadOnly _worldData As WorldData
-    Private ReadOnly _data As MapData
     Public Sub New(worldData As WorldData, mapData As MapData, name As String)
+        MyBase.New(worldData, mapData)
         Me.Name = name
-        _worldData = worldData
-        _data = mapData
     End Sub
     Public ReadOnly Property Name As String Implements IMap.Name
     Public Function GetCell(mapColumn As Integer, mapRow As Integer) As IMapCell Implements IMap.GetCell
-        If mapColumn < 0 OrElse mapRow < 0 OrElse mapColumn >= _data.Columns OrElse mapRow >= _data.Rows Then
-            Return New MapCell(_worldData, New MapCellData With
+        If mapColumn < 0 OrElse mapRow < 0 OrElse mapColumn >= Data.Columns OrElse mapRow >= Data.Rows Then
+            Return New MapCell(WorldData, New MapCellData With
                                {
-                                .Terrain = _data.DefaultTerrain
+                                .Terrain = Data.DefaultTerrain
                                })
         End If
-        Return New MapCell(_worldData, _data.Cells(mapColumn + mapRow * _data.Columns))
+        Return New MapCell(WorldData, Data.Cells(mapColumn + mapRow * Data.Columns))
     End Function
 End Class
