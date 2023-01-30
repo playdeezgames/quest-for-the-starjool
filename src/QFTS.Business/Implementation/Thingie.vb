b1e241ee50929bc84f90277db1ad1ad5f90249bf
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 05:42:03 -0600

    thingie exists

---
+++ src/QFTS.Business/Implementation/Thingie.vb
@@ -0,0 +1,18 @@
+﻿Friend Class Thingie(Of TData)
+    Private ReadOnly _worldData As WorldData
+    Private ReadOnly _data As TData
+    Protected ReadOnly Property WorldData As WorldData
+        Get
+            Return _worldData
+        End Get
+    End Property
+    Protected ReadOnly Property Data As TData
+        Get
+            Return _data
+        End Get
+    End Property
+    Sub New(worldData As WorldData, data As TData)
+        _worldData = worldData
+        _data = data
+    End Sub
+End Class
