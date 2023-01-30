b35036037bbceeb4800ac5c864b923e97a787ef1
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-01-30 08:28:32 -0600

    rearrange

---
+++ src/QFTS.Business/Extensions/ItemTypeExtensions.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+
+Friend Module ItemTypeExtensions
+    Private ReadOnly table As IReadOnlyDictionary(Of ItemType, ItemTypeDescriptor) =
+        New Dictionary(Of ItemType, ItemTypeDescriptor) From
+        {
+            {ItemType.Club, New ItemTypeDescriptor}
+        }
+    <Extension>
+    Function ToDescriptor(itemType As ItemType) As ItemTypeDescriptor
+        Return table(itemType)
+    End Function
+End Module
